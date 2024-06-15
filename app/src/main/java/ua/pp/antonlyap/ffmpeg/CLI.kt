50d2ed3b68b145c113bd3cdc84359cc14aec2fa1
Anton <anton@liap.xyz>
2024-06-15 23:30:16 +0300

    Support passing FFmpeg options as env variable

--- app/src/main/java/ua/pp/antonlyap/ffmpeg/CLI.kt
+++ app/src/main/java/ua/pp/antonlyap/ffmpeg/CLI.kt
@@ -12,13 +12,16 @@ class CLI {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
+            val envOptions = System.getenv("FFMPEG_OPTIONS")?.split(" ")?.toTypedArray()
+            val argOptions = args.drop(1).toTypedArray()
+            val options = envOptions ?: argOptions
             if (args[0].endsWith("ffmpeg")) {
                 FFmpegKitConfig.enableLogCallback { handleLog(it) }
-                val session = FFmpegKit.executeWithArguments(args.drop(1).toTypedArray())
+                val session = FFmpegKit.executeWithArguments(options)
                 Thread.sleep(200)
                 exitProcess(session.returnCode.value)
             } else if (args[0].endsWith("ffprobe")) {
-                val session = FFprobeKit.executeWithArguments(args.drop(1).toTypedArray())
+                val session = FFprobeKit.executeWithArguments(options)
                 print(session.output)
                 exitProcess(session.returnCode.value)
             } else {
